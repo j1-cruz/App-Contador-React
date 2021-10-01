938a4eb1e8ee7cb5e1a502aae35b449d18e02d85
j1-cruz <membrillajc@gmail.com>
2021-10-01 11:30:18 -0300

    Add files via upload

---
+++ componets/Contador.js
@@ -0,0 +1,29 @@
+const Contador = () => {
+
+    const [contador, setContador] = React.useState(0);
+    
+    const Aumentar = () => setContador(contador + 1)
+    const Disminuir = () => setContador(contador - 1)
+
+    return (
+        <div>
+         <h1 className={ contador > 0 ? "mayor" : "menor"}>Contador: {contador}</h1>
+
+            <hr/>
+            <button onClick={Disminuir}>Disminuir</button>
+            <button  onClick={Aumentar}>Aumentar</button>
+        </div>
+   
+    ) 
+}
+/*
+return (
+    <div>
+        <h1 className={ contador < 0 ? "menor" : "mayor"}>Contador: {contador}</h1>
+        <hr/>
+        <button onClick={Disminuir}>Disminuir</button>
+        <button  onClick={Aumentar}>Aumentar</button>
+    </div>
+
+) 
+*/
